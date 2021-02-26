1a307efc2f63a504151e5bf5a71c4927fe17e2e0
gushimaaceline <celinegushimaa@gmail.com>
2021-02-26 07:02:58 +0300

    Assignment_Correction

---
+++ src/Almni.kt
@@ -0,0 +1,27 @@
+fun main() {
+    add()
+    me()
+    name()
+    add()
+}
+
+fun add() {
+var a= 10
+    var b= 20
+    var sum= a+b
+    println(sum)
+
+}
+fun me () {
+    var name="Gushuma Celinene"
+    println(name)
+}
+fun name() {
+    var name= "loveLace"
+    println(name)
+}
+fun add(a:Int,b:Int):Int{
+    return a + b
+
+}
+
